13c33db8f370db87ee4d299b711a1156c7167537
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-11-04 16:53:29 +0300

    added launches endpoint

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -9,11 +9,9 @@ async function httpGetPlanets() {
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // const response = await fetch(`${API_URL}/launches`);
-  // const fetchedLaunches = await response.json();
-  // return fetchedLaunches.sort((a, b) => {
-  //   return a.flightNumber - b.flightNumber;
-  // });
+  const response = await fetch(`${API_URL}/launches`);
+  const launches = await response.json();
+  return launches.sort((a, b) => a.flightNumber - b.flightNumber)
 }
 
 // Submit given launch data to launch system.
--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -35,9 +35,11 @@ function loadPlanetsData() {
 
 }
 
-
+function getAllPlanets() {
+    return habitablePlanets
+}
 
 module.exports = {
     loadPlanetsData,
-    planets: habitablePlanets
+    getAllPlanets
 }
