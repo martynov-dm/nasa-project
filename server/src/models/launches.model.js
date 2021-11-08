5cdaa6e4ac17fa2b19cdfc12fe1574cf1aeb178a
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-11-08 11:58:01 +0300

    added post handler

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map()
 
+let latestFlightNumber = 100
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -13,10 +15,25 @@ const launch = {
 
 launches.set(launch.flightNumber, launch)
 
-function getLaunches() {
+function getAllLaunches() {
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber ++
+    const newLaunch = {
+        ...launch,
+        flightNumber: latestFlightNumber,
+        customers: ['ZTM', 'NASA'],
+        upcoming: true,
+        success: true
+    }
+
+    launches.set(latestFlightNumber, newLaunch)
+    return newLaunch
+}
+
 module.exports = {
-    getLaunches,
+    getAllLaunches,
+    addNewLaunch
 }
